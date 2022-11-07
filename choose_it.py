c109368bfefdb46bc68132ae363428c0865e21b3
Sean Bannon <seannbannon@gmail.com>
2022-11-07 15:46:46 -0600

    changing what i had to functions

--- choose_it.py
+++ choose_it.py
@@ -1,12 +1,36 @@
 name = input("What's your name?")
-print("Welcome to the forests of Avalon", name,"!")
 
-answer = input(
-    "You have fallen through the purple swiriling wormhole of Avalon and have found yourself back in the Babylon Jungle. Home to some of the most deadly creatures on the planet. As you dust yourself off from your abrupt wormhole suck and begin to get your bearings you pick up your weapon. What was your weapon, again? (sword, shield, magic wand)").lower()
+def start():
+    print("")
+    print("Welcome to the forests of Avalon", name,"!")
+    print("")
+    pickWeapon()
+
+def pickWeapon():
+    print("")
+    weaponChoice = input("You have fallen through the purple swiriling wormhole of Avalon and have found yourself back in the Babylon Jungle. Home to some of the most deadly creatures on the planet. As you dust yourself off from your abrupt wormhole suck and begin to get your bearings you pick up your weapon. What was your weapon, again? (sword, shield, magic wand)")
+    if weaponChoice == "sword" or pickWeapon == "shield" or pickWeapon == "magic wand":
+        print("")
+        print("Oh right! You fight with a {weaponChoice}")
+    else:
+        print("Sorry, I didn't catch that, what is your weapon again?")
+        weaponChoice()
 
 
-if answer == "sword" or "shield" or "magic wand":
-    print(f'Oh, right! You fight with a {answer}!')
-else:
-    print("Sorry, I didn't catch that. What was your weapon again?")
+start()
+
+
+
+
+
+
+
+# weapon = input(
+#     "You have fallen through the purple swiriling wormhole of Avalon and have found yourself back in the Babylon Jungle. Home to some of the most deadly creatures on the planet. As you dust yourself off from your abrupt wormhole suck and begin to get your bearings you pick up your weapon. What was your weapon, again? (sword, shield, magic wand)").lower()
+
+
+# if weapon == ("sword" or "shield" or "magic wand"):
+#     print(f'Oh, right! You fight with a {weapon}!')
+# else:
+#     print("Sorry, I didn't catch that. What was your weapon again?")
 
