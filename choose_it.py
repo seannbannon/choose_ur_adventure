3c7ec8011a266a29ec98552ba27097ec5acc8af8
Sean Bannon <seannbannon@gmail.com>
2022-11-10 14:30:51 -0600

    another chapter added

--- choose_it.py
+++ choose_it.py
@@ -24,13 +24,21 @@ def hearNoise():
     noiseChoice = input("You get your mind in order, put your " + weaponChoice + " on your belt and brush the dust off your cap. As you stand up you hear a rustling in the bushes behind you. Do you want to investigate it or run away?(investigate/run)")
     if noiseChoice == "investigate":
         print("")
-        print("You go peer behind the bushes and find a small baby gnome wrapped in a red blanket. He looks up at you and giggles. Cute little defenseless fella. Can't be safe around here for him. Should you take him with you or leave him behind?")
+        # print("You go peer behind the bushes and find a small baby gnome wrapped in a red blanket. He looks up at you and giggles. Cute little defenseless fella. Can't be safe around here for him. Should you take him with you or leave him behind? (take him/ leave him)")
+        gnome()
     elif noiseChoice =="run":
         print("Your cowardice has gotten the best of you. You turn in the opposite direction and run into the darkness toward a small firelight you see off in the distance.")
     else:
         print("Sorry, I didn't catch that, what is your answer again?")
         print("")
         hearNoise()
+
+def gnome():
+    print("")
+    gnomeChoice = input("You go peer behind the bushes and find a small baby gnome wrapped in a red blanket. He looks up at you and giggles. Cute little defenseless fella. Can't be safe around here for him. Should you take him with you or leave him behind? (take him/ leave him)")
+    if gnomeChoice == "take him":
+        print("")
+        print("You listened to your heart and decided to take the baby gnome with you. You tie him to your back and carry on down the path. You head north-- as you recall a small town from your previous visit-- which has a wormhole of their own that you can take home. ")
 
 
 start()
