9a37b09edfef3562f684e1527d89baaf19aa0657
Sean Bannon <seannbannon@gmail.com>
2022-11-12 14:13:00 -0600

    comments

--- choose_it.py
+++ choose_it.py
@@ -44,7 +44,7 @@ def gnome():
 
 def firelight():
     print("")
-    fireChoice = input("As you get close to the firelight, something seems strange. The fire begins to burst into different colored light, and see figures around the fire chanting something in a strange tongue you've never heard before. They begin to beat a large drum and you can see them carrying out what seems to be a sacrifice. They begin to tie what looks like a young child above the fire. Should you cause a distraction and possibly save the child or sneak away?(distract/sneak away")
+    fireChoice = input("As you get close to the firelight, something seems strange. The fire begins to burst into different colored light, and see figures around the fire chanting something in a strange tongue you've never heard before. They begin to beat a large drum and you can see them carrying out what seems to be a sacrifice. They begin to tie what looks like a young child above the fire. Should you cause a distraction and possibly save the child or sneak away?(distract/sneak away)")
     if fireChoice == "distract":
         print("")
         print
@@ -55,18 +55,4 @@ def firelight():
 
 start()
 
-
-
-
-
-
-
-# weapon = input(
-#     "You have fallen through the purple swiriling wormhole of Avalon and have found yourself back in the Babylon Jungle. Home to some of the most deadly creatures on the planet. As you dust yourself off from your abrupt wormhole suck and begin to get your bearings you pick up your weapon. What was your weapon, again? (sword, shield, magic wand)").lower()
-
-
-# if weapon == ("sword" or "shield" or "magic wand"):
-#     print(f'Oh, right! You fight with a {weapon}!')
-# else:
-#     print("Sorry, I didn't catch that. What was your weapon again?")
 
