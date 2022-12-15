6fefef05886d98f537c9ab5a9d48f659892a468f
Sean Bannon <seannbannon@gmail.com>
2022-12-15 10:46:36 -0600

    adding nested if statements to fire battle scene

--- choose_it.py
+++ choose_it.py
@@ -36,19 +36,26 @@ def hearNoise():
         hearNoise()
 
 def gnome():
+    global gnomeChoice
     print("")
     gnomeChoice = input("You go peer behind the bushes and find a small baby gnome wrapped in a red blanket. He looks up at you and giggles. Cute little defenseless fella. Can't be safe around here for him. Should you take him with you or leave him behind? (take him/ leave him)")
     if gnomeChoice == "take him":
         print("")
-        print("You listened to your heart and decided to take the baby gnome with you. You tie him to your back and carry on down the path. You head north-- as you recall a small town from your previous visit-- which has a wormhole of their own that you can take home. ")
+        print("You listened to your heart and decided to take the baby gnome with you. You tie him to your back and carry on down the path. You head north-- as you recall a small town from your previous visit-- which has a wormhole of their own that you can take home. On the way, through the darkness- you see a campfire light. You figure perhaps the people there can help you figure out which direction the town is in.")
+        firelight()
+    elif gnomeChoice == "leave him":
+        print("")
+        print("You decided to leave the gnome. You can't find your way out of here while worrying about a baby gnome. Besides, I'm sure his mother will be back for him in a moment. Like a deer. As you turn around you see a campfire off in the distance. You find yourself absolutely mezmerized by it. You head towards the fire.")
+        firelight()
 
 def firelight():
     print("")
     fireChoice = input("As you get close to the firelight, something seems strange. The fire begins to burst into different colored light, and you see figures around the fire chanting something in a strange tongue you've never heard before. They begin to beat a large drum and you can see them carrying out what seems to be a sacrifice. They begin to tie what looks like a young child above the fire. Should you cause a distraction and possibly save the child or sneak away?(distract/sneak away)")
     if fireChoice == "distract":
         print("")
-        print
-
+        if gnomeChoice == "take him":
+            print("")
+            print("You tighten the baby gnome on your back and take out your " + weaponChoice  +" You jump on top of one of the stumps around the fire and swing your " + weaponChoice + " at one of the big guys nearby." )
 
 
 
