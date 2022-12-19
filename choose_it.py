6c9b4caf81d43856b18c70a32646741c09688660
Sean Bannon <seannbannon@gmail.com>
2022-12-19 08:02:53 -0600

    added sneak away choice aas well as the battlescene function

--- choose_it.py
+++ choose_it.py
@@ -12,7 +12,7 @@ def pickWeapon():
     weaponChoice = input("You have fallen through the purple swiriling wormhole of Avalon and have found yourself back in the Babylon Jungle. Home to some of the most deadly creatures on the planet. As you dust yourself off from your abrupt wormhole suck and begin to get your bearings you pick up your weapon. What was your weapon, again? (sword, shield, magic wand)")
     if weaponChoice == "sword" or weaponChoice == "shield" or weaponChoice == "magic wand":
         print("")
-        print("Oh right! You fight with a", weaponChoice, "!")
+        print("Oh right! You fight with a", weaponChoice + "!")
     else:
         print("Sorry, I didn't catch that, what is your weapon again?")
         print("")
@@ -48,6 +48,20 @@ def gnome():
         print("You decided to leave the gnome. You can't find your way out of here while worrying about a baby gnome. Besides, I'm sure his mother will be back for him in a moment. Like a deer. As you turn around you see a campfire off in the distance. You find yourself absolutely mezmerized by it. You head towards the fire.")
         firelight()
 
+
+def battlescene():
+    print("")
+    print("You jump on top of one of the stumps around the fire and swing your " + weaponChoice + " at one of the big guys nearby.")
+    if weaponChoice == "sword":
+        print("")
+        print("You manage to slice the cheek of one of the largest elves. He is unphased and pissed off. He leaps at you- blood flooding from his sunken face- and you thrust your sword deep into his thigh. He goes down and lets out a mighty yell. Everyone freezes. Clearly they have a mighty warrior before them. They wait for you to speak.")
+    if weaponChoice == "magic wand":
+        print("")
+        print("A bolt of lightening flies from your wand- strinking the boss elf between the eyes. He does down hard. Everyone freezes. They have't seen a wizard in over 400 years. They wait for you to to speak.")
+    if weaponChoice == "shield":
+        print("")
+        print("You smash the face of the largest elf. He falls backwards into the fire. Another one charges you from behind, knocking you down. He jumps on top of you, but you manage to push him off with your shield. You scuffle up to your feet and thrust the bottom of the shield into the elf's throat. Decapitating him. Everyone halts and stares at you. They wait for you to say something.")
+
 def firelight():
     print("")
     fireChoice = input("As you get close to the firelight, something seems strange. The fire begins to burst into different colored light, and you see figures around the fire chanting something in a strange tongue you've never heard before. They begin to beat a large drum and you can see them carrying out what seems to be a sacrifice. They begin to tie what looks like a young child above the fire. Should you try to save the child or sneak away?(save/sneak away)")
@@ -55,20 +69,15 @@ def firelight():
         print("")
         if gnomeChoice == "take him":
             print("")
-            print("You tighten the baby gnome on your back and take out your " + weaponChoice  +" You jump on top of one of the stumps around the fire and swing your " + weaponChoice + " at one of the big guys nearby." )
-            if weaponChoice == "sword":
-                print("")
-                print("You manage to slice the cheek of one of the largest elves. He is unphased and pissed off. He leaps at you- blood flooding from his sunken face- and you thrust your sword deep into his thigh. He goes down and lets out a mighty yell. Everyone freezes. Clearly they have a mighty warrior before them. They wait for you to speak.")
-            if weaponChoice == "magic wand":
-                print("")
-                print("A bolt of lightening flies from your wand- strinking the elf between the eyes. He does down hard. Everyone freezes. They have't seen a wizard in over 400 years. They wait for you to to speak.")
-            if weaponChoice == "shield":
-                print("")
-                print("You smash the face of the largest elf. He falls backwards into the fire. Another one charges you from behind, knocking you down. He jumps on top of you, but you manage to push him off with your shield. You scuffle up to your feet and thrust the bottom of the shield into the elf's throat. Decapitating him. Everyone halts and stares at you. They wait for you to say something.")
+            print("You tighten the baby gnome on your back and take out your " + weaponChoice + ".")
+            battlescene()
         if gnomeChoice == "leave him":
             print("")
             print("This must be why that baby gnome was in the bushes. Someone hid it there to avoid it being used as a human sacrifice.")
-
+            battlescene()
+    if fireChoice == "sneak away":
+        print("")
+        print("You decide it would probably be best to let sleeping dogs lie. You pull up your hood and creep through the shadows to avoid detection. You feel bad for leaving that child to die, and you should. However, it's not up to you to save the world. You aren't entirely sure how you even got here and you're focused on making it back home in one piece. You are a coward, though, and def do not have what it takes to be a great adventurer. ")
 
 
 start()
